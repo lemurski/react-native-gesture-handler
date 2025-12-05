b1ae80dfe25bb97760d3ae065a6da2fb5c27e993
kacperlem <kacperlem@gmail.com>
2025-12-05 15:46:49 +0100

    first batch

---
+++ lib/typescript/__mocks__/RNGestureHandlerModule.d.ts
@@ -0,0 +1,41 @@
+declare const _default: {
+    TouchableHighlight: import("react").ForwardRefExoticComponent<import("react-native").TouchableHighlightProps & import("react").RefAttributes<import("react-native").View>>;
+    TouchableNativeFeedback: typeof import("react-native").TouchableNativeFeedback;
+    TouchableOpacity: import("react").ForwardRefExoticComponent<import("react-native").TouchableOpacityProps & import("react").RefAttributes<import("react-native").View>>;
+    TouchableWithoutFeedback: typeof import("react-native").TouchableWithoutFeedback;
+    ScrollView: typeof import("react-native").ScrollView;
+    FlatList: typeof import("react-native").FlatList;
+    Switch: typeof import("react-native").Switch;
+    TextInput: typeof import("react-native").TextInput;
+    DrawerLayoutAndroid: typeof import("react-native").DrawerLayoutAndroid;
+    NativeViewGestureHandler: typeof import("react-native").View;
+    TapGestureHandler: typeof import("react-native").View;
+    ForceTouchGestureHandler: typeof import("react-native").View;
+    LongPressGestureHandler: typeof import("react-native").View;
+    PinchGestureHandler: typeof import("react-native").View;
+    RotationGestureHandler: typeof import("react-native").View;
+    FlingGestureHandler: typeof import("react-native").View;
+    PanGestureHandler: typeof import("react-native").View;
+    attachGestureHandler: () => void;
+    createGestureHandler: () => void;
+    dropGestureHandler: () => void;
+    updateGestureHandler: () => void;
+    flushOperations: () => void;
+    install: () => void;
+    Directions: {
+        readonly RIGHT: 1;
+        readonly LEFT: 2;
+        readonly UP: 4;
+        readonly DOWN: 8;
+    };
+    State: {
+        readonly UNDETERMINED: 0;
+        readonly FAILED: 1;
+        readonly BEGAN: 2;
+        readonly CANCELLED: 3;
+        readonly ACTIVE: 4;
+        readonly END: 5;
+    };
+};
+export default _default;
+//# sourceMappingURL=RNGestureHandlerModule.d.ts.map
